a85d862142436fe5ae82bc224fcbc3c3f978f147
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-10-02 19:49:14 -0400

    css done, started js

---
+++ main.js
@@ -0,0 +1,16 @@
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
