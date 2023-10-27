d04cf4be2da21aedcc0c90e1448f3abdad330cc1
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-10-27 16:45:34 -0400

    created react function and move over elements

---
+++ App.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div>
+      <h1>Tic-Toe-React</h1>
+      <h1>Tic-Tac-Toe</h1>
+      <h2>It's X's turn!</h2>
+      <div class="flex-container flex-column">
+        <div class="flex-container flex-wrap" id="board">
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+        </div>
+        <button id="reset-button">reset</button>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, root);
